af3a50dc13873f59d20b7f3b9be2cf598ecd65d8
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-10-25 22:01:44 -0400

    End of class
    Added database; some additional coding needed to format answers

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,10 +3,17 @@ class GuessesController < ActionController::Base
 def index
   # params = ["frist_number" => "4", "second_number" => "5" etc.]
 
-  @first = params["first_number"]
-  @second = params["second_number"]
-  @third = params["third_number"]
+  # @first = params["first_number"]
+  # @second = params["second_number"]
+  # @third = params["third_number"]
 
+g = Guess.new
+g.first_num = params['first_number']
+g.second_num = params['second_number']
+g.third_num = params['third_number']
+g.save
+
+@list = Guess.all
 render("guesses/index.html.erb")
 end
 
