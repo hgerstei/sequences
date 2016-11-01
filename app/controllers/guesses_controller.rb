334fc3fe03af8b6b909cff4a83130a9cfc4cf6d3
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-01 09:49:48 -0400

    Revert "Pre-revert commit"
    This reverts commit 9905e7a4890e89bfef41a98a1fec4b43ee094ae6.

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,14 +2,17 @@ class GuessesController < ApplicationController
   def index
     # params = {"first_number"=>"4", "second_number"=>"5", "third_number"=>"6"}
 
-    g = Guess.new.grep(integer)
-    g.first_num = params["first_number"]
-    g.second_num = params["second_number"]
-    g.third_num = params["third_number"]
-    g.save
+g = Guess.new
+g.first_num = params[:first_number]
+g.second_num = params[:second_number]
+g.third_num = params[:third_number]
+g.save
 
-    @list = Guess.all
+@list = Guess.all
 
+if g.first_num == nil || g.second_num == nil || g.third_num == nil
+  puts ""
+end
     render("guesses/index.html.erb")
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "guesses", :action => "index" })
+
   get("/all_guesses", { :controller => "guesses", :action => "index" })
 
   get("/show_answer", { :controller => "guesses", :action => "answers" })
