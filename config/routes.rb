9905e7a4890e89bfef41a98a1fec4b43ee094ae6
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-01 09:40:21 -0400

    Pre-revert commit
    Saving interim changes but ultimately plan to revert to old version of
    the file with few errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "guesses", :action => "index" })
-
   get("/all_guesses", { :controller => "guesses", :action => "index" })
 
   get("/show_answer", { :controller => "guesses", :action => "answers" })
